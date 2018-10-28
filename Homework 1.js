1a6b6647dce0121dee733dba52e4b98b2c00faec
Marwo7 <44320286+Marwo7@users.noreply.github.com>
2018-10-28 10:00:42 +0100

    Add files via upload

---
+++ Homework 1.js
@@ -0,0 +1,146 @@
+// 3) done
+// let numbers = [1,6,23,8,4,98,3,7,3,98,4,98]
+
+// let number
+
+// let sum = 0
+
+// for (let i = 0; i < numbers.length; i++) {
+//     number=numbers[i]
+
+//     if (number%2!==0) {
+//         sum += number
+//     }
+    
+// }
+// console.log(sum)
+
+//4) done
+// let numArray = [1,6,23,8,4,98,3,7,3,98,4,98]
+
+// let lowestValue
+
+// let highestValue
+
+// for (let i = 0; i < numArray.length; i++) {
+//     lowestValue = numArray[0]
+//     highestValue = numArray[0]
+//     if (lowestValue > numArray[i]) {
+//         lowestValue = numArray[i]
+//     } else if (highestValue < numArray[i]) {
+//         highestValue = numArray[i]
+//     } {
+        
+//     }
+    
+// }
+    
+// console.log('The highest value is ' +highestValue +', the lowest value is ' +lowestValue)
+
+//5) done
+
+// let strArray = ["Karol", 'Adam','Rogowski','Politechnika','Super','Weekend']
+
+// let lgth = 0;
+// let longest;
+
+// for(let i=0; i < strArray.length; i++){
+//     if(strArray[i].length > lgth){
+//         lgth = strArray[i].length;
+//         longest = strArray[i];
+//     }      
+// } 
+
+// console.log(longest);
+
+//6) done
+
+// let values = [1,6,23,8,4,98,3,7,3,98,4,98]
+
+// let highestValue ;
+// let highestValueIndex = [];
+// for (let i = 0; i < values.length; i++) {
+//     highestValue = values[i]
+//     if (values[i] > highestValue) {
+//         highestValue = values[i];
+//     };
+// };
+// for (let i = 0; i < values.length; i++) {
+//     if (values[i] === highestValue) {
+//         highestValueIndex.push(i);
+//     };
+// };
+
+// console.log('The highest value is ' + highestValue + ' and the indexes are ' + highestValueIndex)
+
+//7) done
+// let evenNumbers = [1,6,23,8,4,98,3,7,3,98,4,98]
+
+// let evenNumber
+
+// let sum = 0
+
+// let evenArray =[]
+
+// let average
+
+// for (let i = 0; i < evenNumbers.length; i++) {
+//     evenNumber = evenNumbers[i]
+
+// if (evenNumber%2==0) {
+//     sum += evenNumber
+//     evenArray.push(evenNumber)
+//     average = sum/evenArray.length
+// }
+// }
+// console.log(average)
+
+//8) done
+// let evenNumbers = [1,6,23,8,4,98,3,7,3,98,4,98]
+
+// let evenNumber
+
+// let sum = 0
+
+// let evenArray =[]
+
+// let average
+
+// for (let i = 2; i < evenNumbers.length; i++) {
+//     evenNumber = evenNumbers[i]
+
+// if ((i%2)===0) {
+//     sum += evenNumber
+//     evenArray.push(evenNumber)
+//     average = sum/evenArray.length
+// }
+// }
+// console.log(average)
+
+//9a)even and odd indexes
+// let numbers = [1,6,23,8,4,98,3,7,3,98,4,98] 
+
+// let result = 0
+
+// for (let i = 0; i < numbers.length; i++) {
+//     if (i%2==0) {
+//         result += numbers[i]
+//     } else {
+//         result -= numbers[i]
+//     }
+// }
+// console.log(result)
+
+//9b) even and odd numbers
+// let numbers = [1,6,23,8,4,98,3,7,3,98,4,98] 
+
+// let result = 0
+
+// for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i]%2==0) {
+//         result += numbers[i]
+//     } else {
+//         result -= numbers[i]
+//     }
+// }
+// console.log(result)
